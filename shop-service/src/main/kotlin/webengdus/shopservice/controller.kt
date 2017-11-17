a449163fb542bfe4976a6870cd9ada0ad5ecb691
Mario Mueller <mario@xenji.com>
2017-11-17 06:19:10 +0100

    Renamed SP class

--- shop-service/src/main/kotlin/webengdus/shopservice/controller.kt
+++ shop-service/src/main/kotlin/webengdus/shopservice/controller.kt
@@ -3,7 +3,6 @@ package webengdus.shopservice
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.context.request.RequestContextHolder
